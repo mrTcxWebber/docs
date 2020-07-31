b2da17e74fcd651280a04519b43c2983f2567371
mrt <814463173@qq.com>
2020-07-31 20:53:52 +0800

    feat: add基础部分

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,41 +5,59 @@ module.exports = {
     base: '/docs/',
     repo: 'https://github.com/su37josephxia/frontend-basic',
     title: 'va管理后台文档',
-    plugins: [
-        [
-            '@vuepress/last-updated',
-            {
-                transformer: (timestamp, lang) => {
-                    return moment(timestamp).fromNow()
-                }
-            }
-        ]
-    ],
+    plugins: {
+        '@vuepress/last-updated': {
+            transformer: (timestamp, lang) => moment(timestamp).fromNow()
+        },
+        // "vuepress-plugin-auto-sidebar": {
+        //     titleMode: "uppercase",
+        //     titleMap: {
+        //         javascript: "JS 基础2",
+        //     },
+        //     collapseList: [
+        //         "/frontend/javascript/"
+        //     ]
+        // }
+        // '@vuepress/back-to-top': true
+    },
     themeConfig: {
+        displayAllHeaders: true,
         logo: '/assets/img/0qejd7.jpg',
         lastUpdated: '更新时间',
         smoothScroll: true,
         nav: [
             { text: '首页', link: '/' },
-            { text: '指南', link: '/guide.html' },
-            { text: 'api', link: '/api.html' },
-            // {
-            //     text: 'github',
-            //     items: [
-            //         { text: '下拉', link: '/' },
-            //         { text: '下拉2', link: '/' },
-            //     ]
-            // }
+            { text: '指南', link: '/guide/' },
+            // { text: '前端', link: '/frontend/javascript/' },
+            { text: 'api', link: '/api/' }
         ],
-        // sidebar: {
-        //     '/指南/': [
-        //         'one',
-        //         'two'
-        //     ],
-        //     '/api/': [
-        //         'three',
-        //         'four'
-        //     ]
-        // }
+        sidebar: {
+            '/guide/': [
+                '',
+                'instruction',
+                'base/vue',
+                'base/element',
+                'base/vuex',
+                'base/vue-element-admin',
+                // {
+                //     title: 'Vue进阶',
+                //     path: '/guide/base/vue',
+                //     collapsable: false,
+                //     sidebarDepth: 2,
+                //     children: [
+                //         '/guide/base/vue',
+                //         '/guide/base/element'
+                //     ]
+                // }
+            ],
+
+            // fallback
+            '/': [
+                '',
+            ]
+        }
+    },
+    markdown: {
+        lineNumbers: true
     }
 }
